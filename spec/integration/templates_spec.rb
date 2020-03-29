98c97b4826f4b7bc15bd644bfe0ece2dd6ab5487
Joe Osburn <joe@jnodev.com>
2020-03-28 22:53:43 -0500

    New block spec

--- spec/integration/templates_spec.rb
+++ spec/integration/templates_spec.rb
@@ -20,4 +20,20 @@ describe 'Templates' do
     )
     expect(engine.render('block')).to eq('some info - a footer')
   end
+
+  it 'renders blocks inside blocks' do
+    engine = Beard.new(
+      templates: {
+        '/block' => "
+          {{block footer}}
+            a footer
+            {{block sub}}
+              sub{{block name}}bill{{endblock}}info
+            {{endblock}}
+          {{endblock}}
+          {{footer}} -- {{name}} -- {{sub}}"
+      }
+    )
+    expect(engine.render('block').gsub(/\s+/, ' ')).to eq(' a footer -- bill -- subinfo ')
+  end
 end
